1d32a397ca421b223ec8812740c822cb4111443a
Juan David Otalora <otalorajuand@gmail.com>
2022-06-13 16:54:27 -0500

    addition of task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-
-int main() {
+int main(void)
+{
  
-  char str1[] = "\"Programming is like building a multilingual puzzle";
-  puts(str1);
+	char str1[] = "\"Programming is like building a multilingual puzzle";
+	puts(str1);
 
-  return 0;
+  	return (0);
 }
