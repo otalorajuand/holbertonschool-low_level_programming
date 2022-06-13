0cefa2ee8f3440364f4e0e8b603b44490d749d52
Juan David Otalora <otalorajuand@gmail.com>
2022-06-13 18:54:12 -0500

    addition of task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sizes of different variable types.
+ *
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+
+printf("Size of a char: %d byte(s)\n",sizeof(char));
+printf("Size of an int: %d byte(s)\n",sizeof(int));
+printf("Size of a long int: %d byte(s)\n",sizeof(int));
+printf("Size of a long long int: %d byte(s)\n",(unasigned long)sizeof(long));
+printf("Size of a float: %d byte(s)\n",sizeof(float));
+return (0);
+
+}
