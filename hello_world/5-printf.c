f7930a66ac66f345b0af897920d8cb19a8cc02cd
Juan David Otalora <otalorajuand@gmail.com>
2022-06-13 18:13:16 -0500

    addition of task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a sentence
+ *
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
