1d39c3b266d8eb9b236553b0678feade4979bbbe
Juan David Otalora <otalorajuand@gmail.com>
2022-06-13 19:30:50 -0500

    addition of task 8

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-static const char hello[] = "and that piece of art is useful\" - 
+static const char hello[] = "and that piece of art is useful\" - \
 Dora Korpar, 2015-10-19\n";
 fwrite(hello, sizeof(hello) - 1, 1, stdout);
 return (1);
