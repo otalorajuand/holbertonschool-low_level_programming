07edd9d8b3a484e78880c68b1f10cf52ffec36e5
Juan David Otalora <otalorajuand@gmail.com>
2022-06-14 11:58:34 -0500

    addition of task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all letters from the alphabet in lowercase.
+ *
+ * Return: Always returns 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = "a";
+	while (c < z)
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
