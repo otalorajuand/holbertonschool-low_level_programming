64d271edb66bf545d5fab22a49487be631d245da
Juan David Otalora <otalorajuand@gmail.com>
2022-06-13 19:19:57 -0500

    addition of task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a sentence
+ *
+ * Return: Always returns 1.
+ */
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", strlen("and that piece of art is useful" - Dora Korpar, 2015-10-19"));
+return (1);
+}
