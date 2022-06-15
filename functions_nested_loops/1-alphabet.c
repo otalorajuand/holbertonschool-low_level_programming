a95c35f3fe635fb2bd1acde835505b0cb7153b12
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 11:55:25 -0500

    addition of task 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - Prints the letters of the alphabet in lower case.
@@ -11,11 +10,6 @@ int main(void)
 	
 	print_alphabet();
 	return (0);
-}
-
-int _putchar(int char)
-{
-	putchar(char);
 }
 
 void print_alphabet(void)
