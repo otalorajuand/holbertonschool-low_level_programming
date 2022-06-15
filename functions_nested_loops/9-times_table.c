ea2531f4143e71e609474cd2aab55387cf030e9e
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 18:20:55 -0500

    addition of task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the multiplication table of integers.
+ * Return: Void.
+ */
+
+void times_table(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		int j = 0;
+
+		while (m <= 9)
+		{
+			_putchar('0' + (i * j) / 10);
+			_putchar('0' + (i * j) % 10);
+			_putchar(',');
+			_putchar(' ');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
