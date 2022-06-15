e32091ca1c8216a371f92c6263c1ca8ba0904337
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 15:22:56 -0500

    addition of task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the letters of the alphabet in lower case.
+ * 10 times
+ * Return: Always returns void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+	while (i <= 9)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
