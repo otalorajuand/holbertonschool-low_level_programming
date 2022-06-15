0f34b8fd54ac5ce65697160bebb6cd0cab23071d
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 16:08:39 -0500

    addition of task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Calculates the absolute value of a number.
+ * @i: The int to be checked
+ * Return: Returns the absolute value of int.
+ */
+
+int _abs(int i);
+{
+	return (abs(i));
+}
