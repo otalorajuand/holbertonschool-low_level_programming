d4fae830f405ec2afa7f0fe9f6651336a1244111
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 16:43:10 -0500

    addition of task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * jack_bauer - Prints all the minutes of a day.
+ * Return: Void.
+ */
+
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while (i <= 23)
+	{
+		int j = 0;
+
+		while (j <= 5)
+		{
+			int k = 0;
+
+			while (k <= 9)
+			{
+				_putchar(i);
+				_putchar(':');
+				_putchar(j);
+				_putchar(k);
+			}
+			k++;
+		}
+		j++;
+	}
+	i++;
+}
