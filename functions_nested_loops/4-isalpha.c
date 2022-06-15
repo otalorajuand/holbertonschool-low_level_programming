d194eee09301a822a307ac5c5a4efef84fec2d0c
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 15:45:03 -0500

    addition of task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - Check if a character is alphanumeric.
+ * @c: The char to be checked
+ * Return: Return 1 if the char is alphanumeric, or 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha()(c) != 0)
+	{
+		return (1);
+	}
+	return (0);
+}
