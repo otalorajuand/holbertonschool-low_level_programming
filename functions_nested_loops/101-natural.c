63c4b91dfd0c7f49d5d996ea74c5c88e4c0194a6
Juan David Otalora <otalorajuand@gmail.com>
2022-06-16 13:50:33 -0500

    addition of task 13

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the sum of all the multiples of 3 and 5 until 1024.
+ *
+ * Return: Always returns 0.
+ */
+
 int main(void)
 {
 	int i = 0;
@@ -10,8 +16,9 @@ int main(void)
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			n = n + i;
+			i++;
 		}
 	}
-	printf("%d\n",n);
+	printf("%d\n", n);
 	return (0);
 }
