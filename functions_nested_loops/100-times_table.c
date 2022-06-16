9b2713f6da649b4976869a901e608958394e63a2
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 22:03:06 -0500

    addition of task 12

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the nth multiplication table of integers.
+ * @n: the nth table
+ * Return: Void.
+ */
+
+void print_times_table(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		int j = 0;
+
+		while (j <= n)
+		{
+			if (j == 0)
+			{
+				_putchar('0' + (i * j) % 10);
+			}
+			else if ((i * j) / 10 == 0)
+			{
+				_putchar(' ');
+				_putchar('0' + (i * j) % 10);
+			} else
+			{
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
+			}
+
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			j++;
+		}
+		if (i <= n)
+		{
+			_putchar('\n');
+		}
+		i++;
+	}
+}
