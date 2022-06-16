0f7989d3157b02c1e41dad75f55c1d91be6d94d7
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 22:25:43 -0500

    addition of task 13

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * multiples35 - Prints the nth multiplication table of integers.
+ *
+ * Return: Void.
+ */
+
+void multiples35(void)
+{
+	int i = 0;
+	int n = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			n = n + i;
+		}
+	}
+	return (n);
+}
