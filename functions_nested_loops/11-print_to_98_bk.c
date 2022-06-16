35f16449bd3673b7201b4593e1bd36015f4551a5
Juan David Otalora <otalorajuand@gmail.com>
2022-06-15 21:32:49 -0500

    addition of task 11

--- functions_nested_loops/11-print_to_98_bk.c
+++ functions_nested_loops/11-print_to_98_bk.c
@@ -13,27 +13,26 @@ void print_to_98(int n)
 	{
 		if (n < 0)
 		{
-			_putchar('-');
-			if (n > 9)
+			if (abs(n) / 10 == 0)
 			{
-				if (abs(n) / 100 != 0)
-				{
-					_putchar('0' + abs(n) / 100);
-				}
+				_putchar('-');
+				_putchar('0' + abs(n) % 10);
+			} else
+			{
+				_putchar('-');
 				_putchar('0' + abs(n) / 10);
+				_putchar('0' + abs(n) % 10);
 			}
-			_putchar('0' + abs(n) % 10);
 		} else
 		{
-			if (n > 9)
+			if (n / 10 == 0)
 			{
-				if (abs(n) / 100 != 0)
-				{
-					_putchar('0' + n / 100);
-				}
+				_putchar('0' + n % 10);
+			} else
+			{
 				_putchar('0' + n / 10);
+				_putchar('0' + n % 10);
 			}
-			_putchar('0' + n % 10);
 		}
 		if (n < 98)
 		{
