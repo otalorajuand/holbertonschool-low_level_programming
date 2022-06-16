82bcae10a49baf20a5ded0737f818acfb851a868
Juan David Otalora <otalorajuand@gmail.com>
2022-06-16 14:12:55 -0500

    addition of task 14

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -9,14 +9,16 @@
 int main(void)
 {
 	int i = 0;
-	int n1 = 1;
-	int n2 = 2;
+	int n1 = 0;
+	int n2 = 1;
+	int n2aux;
 
 	while (i <= 50)
 	{
-		printf("%d, ", n2);
-		n1 = n2;
+		printf("%d, ", n2 + n1);
+		n2aux = n2;
 		n2 = n2 + n1;
+		n1 = n2aux;
 		i++;
 	}
 	printf("\n");
