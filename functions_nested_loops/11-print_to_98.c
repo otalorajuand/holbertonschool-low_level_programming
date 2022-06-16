db8bf449a897271b929481194baed1689e06c61a
Juan David Otalora <otalorajuand@gmail.com>
2022-06-16 13:27:57 -0500

    addition of task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,34 +9,18 @@
 
 void print_to_98(int n)
 {
-	while (n != 98)
+	if (n < 98)
 	{
-		if (n < 0)
+		while (n < 98)
 		{
-			_putchar('-');
-		}
-		if (n > 9)
-		{
-			if (abs(n) / 100 != 0)
-			{
-				_putchar('0' + abs(n) / 100);
-			}
-			_putchar('0' + n / 10);
-		}
-		_putchar('0' + abs(n) % 10);
-		if (n < 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		} else
-		{
-			_putchar('\n');
-		}
-		if (n <= 98)
-		{
+			printf("%d", n);
 			n++;
-		} else
+		}
+	} else
+	{
+		while (n > 98)
 		{
+			printf("%d", n);
 			n--;
 		}
 	}
