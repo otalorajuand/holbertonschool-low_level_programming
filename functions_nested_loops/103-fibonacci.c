3ca2df4c85b3cb24e10ca435527ed52fc4153cb2
Juan David Otalora <otalorajuand@gmail.com>
2022-06-16 14:22:29 -0500

    addition of task 15

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers.
+ *
+ * Return: Always returns 0.
+ */
+
+int main(void)
+{
+	int i = 0;
+	long counter = 0;
+	long n1 = 0;
+	long n2 = 1;
+	long final_fibo = 4000000;
+	long n2aux;
+
+	while (i < final_fibo)
+	{
+		if ((n2 + n1) % 2 == 0)
+		{
+			counter += (n2 + n1);
+		}
+		n2aux = n2;
+		n2 = n2 + n1;
+		n1 = n2aux;
+		i++;
+	}
+	printf("%ld\n", counter);
+	return (0);
+}
