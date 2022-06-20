acb85a6a0f1851b9f25c99ba0b85abd3d803b94b
Juan David Otalora <otalorajuand@gmail.com>
2022-06-20 18:21:10 -0500

    addition of task 11

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the largest prime factor of the number 612852475143.
+ *
+* Return: Void.
+ */
+
+int check_if_prime(int a);
+
+int main(void)
+{
+	unsigned long int i = 2;
+	unsigned long int number = 612852475143;
+	unsigned long int j;
+	unsigned long int result;
+	
+	while (i <= number / 2)
+	{
+		if (number % i == 0)
+		{
+			if (check_if_prime(i))
+			{
+				result = i;
+			}
+
+		}
+		i++;
+	}
+	printf("%ld\n", result);
+	return (0);
+}
+
+int check_if_prime(int a)
+{
+	int i = 2;
+
+	if (a == 2)
+		return (1);
+
+	while (i < a)
+	{
+		if (a % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+
+}
