3da6db1e319a660d85f6b235d73068f6434e4790
Juan David Otalora <otalorajuand@gmail.com>
2022-06-20 15:17:13 -0500

    addition of task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * print_square - Prints a square made of #
+ * @n: Side of square
+* Return: Void.
+ */
+
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i = size;
+
+		while (i--)
+		{
+			int j = size;
+
+			while (j--)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+}
