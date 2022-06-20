5c6b1464a11c1a3585c313830e222fee811adbaa
Juan David Otalora <otalorajuand@gmail.com>
2022-06-20 14:06:59 -0500

    addition of task 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_line - Prints a line
+ * @n: Length of the line
+* Return: Void.
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n--)
+		{
+			_putchar('_');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+	
+}
