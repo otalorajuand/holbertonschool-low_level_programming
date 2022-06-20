cdb9599a28911eaf416421ad11346a1f25157563
Juan David Otalora <otalorajuand@gmail.com>
2022-06-20 14:45:56 -0500

    addition of task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_diagonal - Prints a diagonal line
+ * @n: Length of the line
+* Return: Void.
+ */
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		while (n--)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+	} else
+	{
+		_putchar('\n');
+	}
+}
