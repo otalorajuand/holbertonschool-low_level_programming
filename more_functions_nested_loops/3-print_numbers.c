ed9eec212ed3a0f8be9d457846f3a357e8c496ab
Juan David Otalora <otalorajuand@gmail.com>
2022-06-20 12:55:31 -0500

    addition of task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9.
+ *
+* Return: Void.
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar('0' + i);
+	}
+}
