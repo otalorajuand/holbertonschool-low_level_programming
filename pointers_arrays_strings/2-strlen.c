703aa763ef17a82645c8e1ecb5b23f5b452003bb
Juan David Otalora <otalorajuand@gmail.com>
2022-06-21 15:03:42 -0500

    addition of task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The pointer of the string.
+* Return: The lenght of the string.
+ */
+
+int _strlen(char *s);
+{
+	return (strlen(*s));
+}
