2186410357110f547be79b2f4a40e20321570296
Juan David Otalora <otalorajuand@gmail.com>
2022-06-21 18:35:46 -0500

    addition of task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - Prints the first n elements of an array.
+ * @a: The pointer of the array.
+ * @n: the number of elements to be printed
+* Return: Nothing.
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%i", a[i]);
+
+		if (i < (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+
+}
