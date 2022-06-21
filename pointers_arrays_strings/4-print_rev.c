646b5eebfb0fb0df2eeaf69eff44345a0250e800
Juan David Otalora <otalorajuand@gmail.com>
2022-06-21 15:40:37 -0500

    addition of task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - Prints a reversed string.
+ * @s: The pointer of the string.
+* Return: Nothing.
+ */
+
+void print_rev(char *s)
+{
+	strrev(s);
+}
