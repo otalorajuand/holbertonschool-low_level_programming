d0b500f70db36bf33fabca9354e113cad26bfc44
Juan David Otalora <otalorajuand@gmail.com>
2022-06-22 17:31:04 -0500

    addition of task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+* Return: The difference of the first two differente chars.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int len = strlen(s1);
+	int i = 0;
+
+	while (i <= len)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
