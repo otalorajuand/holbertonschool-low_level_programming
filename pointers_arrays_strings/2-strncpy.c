faafb8b67ca9d0555deee48f99592a4f39b18836
Juan David Otalora <otalorajuand@gmail.com>
2022-06-22 11:55:07 -0500

    addition of task 2 of day 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncpy - Copies the first n characters of a string into another string.
+ * @src: The pointer of the source string.
+ * @dest: The pointer of the destination string.
+ * @n: Number of characters to concatenate from second string.
+ *
+* Return: The copied destionation string.
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int i;
+
+	if (str != '\0')
+	{
+		for (i = 0; i <= n; i++)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
