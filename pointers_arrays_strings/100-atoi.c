12c033f78d90225c2c775f50b76970fc76bb4f9b
Juan David Otalora <otalorajuand@gmail.com>
2022-06-21 21:40:09 -0500

    addition of task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _atoi - Converts a string into an integer
+ * @s: The pointer of the source string.
+* Return: The integer.
+ */
+
+int _atoi(char *s)
+{
+	int res = 0;
+	int i = 0;
+	int len = strlen(s);
+	int flag = 0;
+
+	while (i < len)
+	{
+		if (s[i] == '-')
+		{
+			flag++;
+		}
+
+		if (s[i] <= '9' && s[i] >= '0')
+		{
+			if (flad % 2)
+			{
+				res = (res * 10) + (s[i] + '0')
+			} else
+			{
+				res = (res * 10) - (s[i] + '0')
+
+			}
+		}
+		i++;
+	}
+	return (res);
+}
