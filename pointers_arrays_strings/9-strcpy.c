319ff4020207d9631e129cb70b3326c11baf03f3
Juan David Otalora <otalorajuand@gmail.com>
2022-06-21 19:03:57 -0500

    addition of task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - Copies a string into another
+ * @src: The pointer of the source string.
+ * @dest: The pointer of the destination string.
+* Return: Nothing.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
