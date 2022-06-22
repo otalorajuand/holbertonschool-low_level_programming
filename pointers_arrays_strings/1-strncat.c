af8476e0878feab1ae13f1fa7ca4cd9b2905105e
Juan David Otalora <otalorajuand@gmail.com>
2022-06-22 10:36:03 -0500

    addition of task 1 from day 2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncat - Concatenates a string into another.
+ * @src: The pointer of the source string.
+ * @dest: The pointer of the destination string.
+ * @n: Number of characters to concatenate from second string.
+ *
+* Return: The concatenated destionation string.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = strlen(dest);
+
+	while (i <= n)
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+	dest[i + 1] = '\0';
+	return (dest);
+}
