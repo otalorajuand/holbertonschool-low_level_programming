5ec9cde15309cd0bb78e21a39a6f16b48ceaeed0
Juan David Otalora <otalorajuand@gmail.com>
2022-06-23 14:55:32 -0500

    addition of task 1 from day 2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -18,10 +18,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	{
 		while (i < n)
 		{
-			dest[i] = &src[i];
+			&dest[i] = &src[i];
 			i++;
 		}
 	}
 
-	return (s);
+	return (dest);
 }
