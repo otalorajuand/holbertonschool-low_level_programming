5d502d735ea16c80ff205f8503c8a16e5245ccee
Juan David Otalora <otalorajuand@gmail.com>
2022-06-24 08:43:57 -0500

    addition of task 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_chessboard - Prints the chessboard.
+ * @a: The pointer of the string.
+* Return:
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 8;
+	int j;
+
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			printf("%c", a[i][j]);
+			j++;
+		}
+		i++;
+		printf('\n');
+	}
+}
