14c253e29e0e6eee8ae3a1d09841b953c3cd1b48
Juan David Otalora <otalorajuand@gmail.com>
2022-06-24 08:57:34 -0500

    addition of task 7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -4,20 +4,22 @@
 
 /**
  * print_chessboard - Prints the chessboard.
- * @a: The pointer of the string.
+ * @a: The pointer of the double array.
 * Return:
  */
 
 void print_chessboard(char (*a)[8])
 {
-	int i = 8;
+	int i = 0;
 	int j;
 
 	while (i < 8)
 	{
+		j=0;
+		
 		while (j < 8)
 		{
-			putchar(a[i][j]);
+			printf("%c", a[i][j]);
 			j++;
 		}
 		i++;
