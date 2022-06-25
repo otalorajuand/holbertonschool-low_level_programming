42dc90ab04f27c2a15ec2fc94902a54eb4d25694
Juan David Otalora <otalorajuand@gmail.com>
2022-06-25 07:05:22 -0500

    addition of task 101

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_number - Prints a number.
+ * @n: number
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int aux = n;
+	int dec = 10;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	while (1)
+	{
+		aux = abs(n);
+		while (aux > dec)
+		{
+			aux /= 10;
+		}
+		dec *= 10;
+		_putchar('0' + aux % 10);
+
+		if (i == 0)
+		{
+			break;
+		}
+	}
+}
