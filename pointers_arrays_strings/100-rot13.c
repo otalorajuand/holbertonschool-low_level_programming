3a446b0c3898a8b672c1adb9bf1ff12a99ef1767
Juan David Otalora <otalorajuand@gmail.com>
2022-06-27 13:54:05 +0000

    addition of task 8 from day 2

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rot13 - Encodes a string into rot13.
+ * @s: Input String.
+ *
+* Return: The pointer to the encoded string.
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	int len = strlen(s);
+
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int len_input = strlen(input);
+
+	i = 0;
+	while (i < len)
+	{
+		j = 0;
+		while (j < len_input)
+		{
+			if (s[i] == input[j])
+			{
+				s[i] = output[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+
+}
