a5f8fb50d65842b92b4e983c0d3c63887a4f09bf
Juan David Otalora <otalorajuand@gmail.com>
2022-06-27 14:17:56 +0000

    addition of task 9 from day 2

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_number - Prints a number.
+ * @n: number
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int aux = n;
+	int dec = 10;
+	int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+	while (1)
+	{
+		i = 0;
+		aux = labs(n);
+		while (aux >= dec)
+		{
+			aux /= 10;
+			i++;
+		}
+		dec *= 10;
+		_putchar('0' + aux % 10);
+
+		if (i == 0)
+		{
+			break;
+		}
+	}
+}
