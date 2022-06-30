28d903b797f4055ed5fca394891ff7af501a6c82
Juan David Otalora <otalorajuand@gmail.com>
2022-06-30 18:35:36 +0000

    addition of task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse, followed by a new line.
+ * @s: the pointer to the string.
+* Returns: Nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*(s + strlen(s)) == '\n')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*(s + strlen(s)));
+	_print_rev_recursion(s - 1);
+}
