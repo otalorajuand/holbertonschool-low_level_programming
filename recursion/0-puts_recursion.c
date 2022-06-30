87beca9edc222f442308e26a3c06589c2fb2ded2
Juan David Otalora <otalorajuand@gmail.com>
2022-06-30 16:25:53 +0000

    addition of task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: the pointer to the string.
+* Returns: Nothing.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	putchar(*s);
+	return (_puts_recursion(s + 1));
+}
