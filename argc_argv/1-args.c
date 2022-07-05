f7ad6d9ab8b492e79bb49933fe2f2f74ebcff3e4
Juan David Otalora <otalorajuand@gmail.com>
2022-07-05 21:10:32 +0000

    addition of task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,11 +3,13 @@
 /**
  * main - prints the number of arguments
  * @argc: arguments counter.
+ * @argv: arguments vector.
 * Return: prints the number of arguments
  */
 
-int main(int argc)
+int main(int argc, char *argv[])
 {
+	char *s = arg[0];
 	printf("%d\n", argc);
 	return (0);
 }
