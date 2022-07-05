6159bd97b20c373dacabc4e76481646c2bbed8bc
Juan David Otalora <otalorajuand@gmail.com>
2022-07-05 21:09:26 +0000

    addition of task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: arguments counter.
+* Return: prints the number of arguments
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc);
+	return (0);
+}
