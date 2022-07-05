98935a7d9a9896312376e45a82679c97468bbe29
Juan David Otalora <otalorajuand@gmail.com>
2022-07-05 22:14:48 +0000

    addition of task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,15 +10,17 @@
 
 int main(int argc, char *argv[])
 {
+	int num1, num2, mult;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int mult = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mult = num1 * num2;
 	printf("%d\n", mult);
 	return (0);
 }
