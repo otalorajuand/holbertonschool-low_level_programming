4a47c12bf4d647a57a3f033e6809b44e32a09ffb
Juan David Otalora <otalorajuand@gmail.com>
2022-07-05 22:13:25 +0000

    addition of task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - prints the function arguments
+ * main - prints the multiplication of arguments
  * @argc: arguments counter.
  * @argv: arguments vector.
 * Return: prints the number of arguments
@@ -9,17 +10,15 @@
 
 int main(int argc, char *argv[])
 {
-	int cero_ascii = 48;
-	int num1 = *argv[1] - cero_ascii;
-	int num2 = *argv[2] - cero_ascii;
-	int mult = num1 * num2;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int mult = num1 * num2;
 	printf("%d\n", mult);
 	return (0);
 }
