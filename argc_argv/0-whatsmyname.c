45b18e28932dee7d45ba4625e25f0e01766656a4
Juan David Otalora <otalorajuand@gmail.com>
2022-07-05 19:02:40 +0000

    addition of task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: arguments counter.
+ * @argv: arguments vector.
+* Return: Nothing
+ */
+
+void main(int argc, char *argv[])
+{
+	printf(argv[0]);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+In this section we explore the argc and argv parameters in C.
