7ba567f7e49eced4b200af050a00779ba5bd4098
Juan David Otalora <otalorajuand@gmail.com>
2022-07-08 21:47:19 +0000

    addition of task 5

---
+++ more_malloc_free/101-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: The arguments counter.
+ * @argv: The arguments vector.
+* Return: The results of the multiplication
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int res;
+
+	res = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d", res);
+	return (0);
+}
