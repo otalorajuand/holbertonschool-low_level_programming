6d5ea9792a5805e73589fa559276d8a20c935e53
Juan David Otalora <otalorajuand@gmail.com>
2022-07-08 16:36:15 +0000

    addition of task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ptr = malloc(nmemb * size);
 
-	if (ptr == NULL || size == 0)
+	if (ptr == NULL || nmemb * size == 0)
 		return (NULL);
 
 	for (i = 0; i < nmemb * size; i++)
