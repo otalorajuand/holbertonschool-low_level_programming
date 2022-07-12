685b455871177556235bf6cc4102d8da17a31b02
Juan David Otalora <otalorajuand@gmail.com>
2022-07-12 20:50:06 +0000

    addition of task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,12 +19,12 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		}
 
-		if (d->age == NULL)
+		if (d->age == 0)
 		{
 			printf("(nil)\n");
 		} else
 		{
-			printf("Age: %s\n", d->age);
+			printf("Age: %f\n", d->age);
 		}
 
 		if (d->owner == NULL)
