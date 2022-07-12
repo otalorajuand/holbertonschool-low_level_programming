d8e674405f783a1eedbbff99e2d995e81521b45f
Juan David Otalora <otalorajuand@gmail.com>
2022-07-12 16:24:11 +0000

    addition of task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * init_dog - Initializes a structure of type dog
+ * @d: The Dogs structure name.
+ * @name: The dog's name.
+ * @age: The dog's age.
+ * @owner: The dog's owner.
+ * Return: Nothing.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ structures_typedef/main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H
+#define _MAIN_H_
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* _MAIN_H_ */
