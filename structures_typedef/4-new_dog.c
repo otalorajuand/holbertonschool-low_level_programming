6f2f15e669aa98b5b22556c44ccd379052be7994
Juan David Otalora <otalorajuand@gmail.com>
2022-07-12 21:12:33 +0000

    addition of task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - Creates a structure of type dog
+ * @name: The dog's name.
+ * @age: The dog's age.
+ * @owner: The dog's owner name.
+ * Return: The new created dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *d;
+
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return (NULL);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
