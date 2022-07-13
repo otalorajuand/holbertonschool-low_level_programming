421f27e852b154a4b4c488d04226797b31bb539f
Juan David Otalora <otalorajuand@gmail.com>
2022-07-13 18:54:50 +0000

    addition of task 1

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,6 +2,7 @@
 #define _FUNCTIONS_TO_POINTERS_H_
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* _FUNCTIONS_TO_POINTERS_H_ */
 
