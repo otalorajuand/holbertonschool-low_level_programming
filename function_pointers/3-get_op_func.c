9713950b877e6350e14bb37d3b121479e0139f52
Juan David Otalora <otalorajuand@gmail.com>
2022-07-14 02:40:07 +0000

    addition of task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -6,19 +6,19 @@
 /**
  * get_op_func - A function that selects the correct operator.
  * @s: The char that defines the operation
- * return: This function returns a pointer to the function that
+ * Return: This function returns a pointer to the function that
  * corresponds to the operator given as a parameter.
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
 	};
 	int i;
 
