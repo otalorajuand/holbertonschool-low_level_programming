58ab1026cad5e2eee4c09c8343084f71efbc132e
Juan David Otalora <otalorajuand@gmail.com>
2022-07-14 18:38:20 +0000

    addition of task 4

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
-	n_bytes = atoi(argv[1]);
+	number_of_bytes = atoi(argv[1]);
 	if (number_of_bytes < 0)
 	{
 		printf("Error\n");
