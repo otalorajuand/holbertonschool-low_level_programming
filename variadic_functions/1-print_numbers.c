b477d16bd19c401c8891201169701f4f2c531569
Juan David Otalora <otalorajuand@gmail.com>
2022-07-15 14:36:28 +0000

    addition of task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Prints all its arguments with a separator.
+ * @n: the number of parameters.
+ * @separator: The separator between the numbers.
+ * Return: Nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(ap);
+}
+
