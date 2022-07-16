90e3a118c88ce12a78fc1efa1827589bd7bfa00e
Juan David Otalora <otalorajuand@gmail.com>
2022-07-16 22:02:26 +0000

    addition of task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -19,34 +19,37 @@ void print_all(const char * const format, ...)
 
 	va_start(ap, format);
 	i = 0;
-	while (i < len)
+	if (format != NULL)
 	{
-		switch (format[i])
+	while (i < len)
 		{
-		case 'c':
-			printf("%s%c", separator, va_arg(ap, int));
-			break;
-		case 'i':
-			printf("%s%d", separator,va_arg(ap, int));
-			break;
-		case 'f':
-			printf("%s%f", separator, va_arg(ap, double));
-			break;
-		case 's':
-			str = va_arg(ap, char *);
-			if (str == NULL)
+			switch (format[i])
 			{
-				printf("(nil)");
+			case 'c':
+				printf("%s%c", separator, va_arg(ap, int));
 				break;
+			case 'i':
+				printf("%s%d", separator,va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+				{
+					printf("%s(nil)", separator);
+					break;
+				}
+				printf("%s%s", separator, str);
+				break;
+			default: 
+				i++;
+				continue;
 			}
-			printf("%s%s", separator, str);
-			break;
-		default: 
+			separator = ", ";
 			i++;
-			continue;
 		}
-		separator = ", ";
-		i++;
 	}
 	printf("\n");
 	va_end(ap);
