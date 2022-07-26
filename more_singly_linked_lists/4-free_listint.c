9e97230db7bb00020d4cf940381ff1adacd3c814
otalorajuand <otalorajuand@gmail.com>
2022-07-26 12:26:28 -0700

    addition of task 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+  * free_listint - frees a listint_t list.
+  * @head: The pointer to the head node.
+ * Return: Nothing.
+  */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head->next;
+		free(head->str);
+		free(head);
+		head = current;
+	}
+}
