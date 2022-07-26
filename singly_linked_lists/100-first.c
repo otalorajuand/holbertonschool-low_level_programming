ab301e8ba9fe46f79bc44c5accbd7ee4e88b1efa
otalorajuand <otalorajuand@gmail.com>
2022-07-26 09:35:59 -0700

    addition of task 5

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,7 +1,10 @@
 #include "lists.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+	
 __attribute__((constructor)) 
 void foo() 
 { 
-	printf (“You\'re beat! and yet, you must allow,\nI bore my house upon my back!\n”); 
+	printf ("You\'re beat! and yet, you must allow,\nI bore my house upon my back!\n"); 
 }
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -21,6 +23,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void foo();
 int _putchar(int);
 
 #endif /* _LISTS_H_ */
