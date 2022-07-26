39834b10b9c6a9b08ed6cc2ff7bfd3a7626ce0f7
otalorajuand <otalorajuand@gmail.com>
2022-07-26 09:59:06 -0700

    addition of task 5

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-	
-__attribute__((constructor)) 
-void foo() 
-{ 
-	printf ("You\'re beat! and yet, you must allow,\nI bore my house upon my back!\n"); 
+
+/**
+  * foo - prints a text before the main() is executed.
+ * Return: Nothing.
+  */
+
+__attribute__((constructor))
+void foo(void)
+{
+	printf("You\'re beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
