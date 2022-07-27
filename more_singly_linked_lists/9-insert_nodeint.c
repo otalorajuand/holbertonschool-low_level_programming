077fee966a49916f4f1f69df262a5e219ed0a910
otalorajuand <otalorajuand@gmail.com>
2022-07-27 09:33:40 -0700

    Addition of task 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -38,5 +38,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = current_node->next;
 	current_node->next = new_node;
 
-	return (&new_node);
+	return (new_node);
 }
