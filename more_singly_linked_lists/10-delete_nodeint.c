8b44eeab5a7b29b9846ce043635338e11f23065e
otalorajuand <otalorajuand@gmail.com>
2022-07-27 16:23:32 -0700

    addition of task 10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	int j = index;
 	listint_t *current_node, *deleted_node;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	current_node = *head;
