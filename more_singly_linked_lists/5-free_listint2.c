1e9d9634a79e81ceaa5899ad09c7acbea7866037
otalorajuand <otalorajuand@gmail.com>
2022-07-27 08:14:37 -0700

    addition of task 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -17,7 +17,7 @@ void free_listint2(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return;
 
-	while (current != NULL)
+	while (*head != NULL)
 	{
 		new_node = *head;
 		*head = (*(head))->next;
