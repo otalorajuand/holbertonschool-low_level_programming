3e410c72acc90bde74abb6b85b36b81a4ef1f1d4
otalorajuand <otalorajuand@gmail.com>
2022-07-27 09:14:20 -0700

    addition of task 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+  * delete_nodeint_at_index - Deletes a new node in the position of idx.
+  * @head: The head node of the list.
+  * @index: the position of the node to delete.
+ * Return: 1 if it succeeded, -1 if it failed.
+  */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *current_node;
+
+	if (head == NULL)
+		return (-1);
+
+	current_node = *head;
+
+	while (i < index - 1)
+	{
+		current_node = current_node->next;
+		i++;
+	}
+
+	current_node->next = current_node->next->next;
+
+	return (1);
+}
