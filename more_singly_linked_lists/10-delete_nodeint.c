6cd128f3fd2c2851828b718009277712929300d3
otalorajuand <otalorajuand@gmail.com>
2022-07-27 16:14:38 -0700

    addition of task 10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
+	int j = index;
 	listint_t *current_node;
 
 	if (head == NULL)
@@ -31,7 +32,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		current_node = current_node->next;
 		i++;
+		j--;
 	}
+
+	if (j != 1)
+		return (-1);
 
 	current_node->next = current_node->next->next;
 
--- more_singly_linked_lists/10-main.c
+++ more_singly_linked_lists/10-main.c
@@ -65,8 +65,8 @@ int main(void)
 	listint_t *head;
 	int n;
 
-	head = _create_list(2, 9, 6);
-	n = delete_nodeint_at_index(&head, 0);
+	head = _create_list(1, 9);
+	n = delete_nodeint_at_index(&head, 10);
 	printf("-> %d\n", n);
 	print_listint(head);
 	_free_listint(head);
