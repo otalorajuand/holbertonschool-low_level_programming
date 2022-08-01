736265be0959f899a162cd4638bf8672f1275d91
otalorajuand <otalorajuand@gmail.com>
2022-08-01 10:04:59 -0700

    addition of task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,18 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
 	int i, len = strlen(b), ascii_zero = 48;
-	int base = pow(2, len - 1);
+	int base = 1;
 
 	if (b == NULL)
 		return (0);
 
-	for (i = 0; i < len; i++)
+	for (i = len - 1; i >= 0; i--)
 	{
 		if ((b[i] - ascii_zero) != 0 && (b[i] - ascii_zero) != 1)
 			return (0);
 
 		res += (b[i] - ascii_zero) * base;
-		base /= 2;
+		base *= 2;
 	}
 	return (res);
 }
