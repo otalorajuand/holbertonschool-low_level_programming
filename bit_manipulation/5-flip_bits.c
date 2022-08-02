c3937098c6f0f5f3eb727a1b2e86273aad5e03d1
otalorajuand <otalorajuand@gmail.com>
2022-08-01 19:32:23 -0700

    addition of task 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * flip_bits - returns the number of bits you would need
+  * to flip to get from one number to another.
+  * @n: The first number to be compared.
+  * @m: The second number to be compared.
+ * Return: The number of bits you would need to flip to
+ * get from one number to another.
+  */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int counter = 0;
+	int comparison = n ^ m;
+
+	while (comparison != 0)
+	{
+		counter += comparison & 1;
+		comparison >>= 1;
+	}
+
+	return (counter);
+}
