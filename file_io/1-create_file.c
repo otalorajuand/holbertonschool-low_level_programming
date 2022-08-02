b79178ce5fe6879f81b2cb1e827f1c731a0f86d4
otalorajuand <otalorajuand@gmail.com>
2022-08-02 12:04:51 -0700

    addition of task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+  * create_file - creates a file.
+  * @filename: The name of the file.
+  * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure.
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	write(fd, text_content, len);
+
+	close(fd);
+
+	return (1);
+}
