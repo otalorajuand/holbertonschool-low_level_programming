5d529dfea6e6188232efa260301b2828dab05d99
otalorajuand <otalorajuand@gmail.com>
2022-08-02 15:14:20 -0700

    Addition of task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,6 +15,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
+
 	file = fopen(filename, "a");
 
 	if (file == NULL)
