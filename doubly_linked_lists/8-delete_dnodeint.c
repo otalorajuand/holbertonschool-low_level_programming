34e6cdb80792a44024182a337de33c98da035776
otalorajuand <otalorajuand@gmail.com>
2022-08-03 12:28:26 -0700

    addition of task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,36 +11,37 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
 	int j = index;
-	dlistint_t *current_node, *deleted_node;
+	dlistint_t *deleted_node;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 
-	current_node = *head;
+	deleted_node = *head;
 
 	if (index == 0)
 	{
 		*head = (*(head))->next;
 		if (*head != NULL)
 			(*(head))->prev = NULL;
-		free(current_node);
+		free(deleted_node);
 		return (1);
 	}
 
-	while (i < index - 1 && current_node->next != NULL)
+	while (i < index  && deleted_node != NULL)
 	{
-		current_node = current_node->next;
+		deleted_node = deleted_node->next;
 		i++;
 		j--;
 	}
 
-	if (j != 1)
+	if (j != 0)
 		return (-1);
 
-	deleted_node = current_node->next;
-	current_node->next = current_node->next->next;
-	if (current_node->next->next != NULL)
-		current_node->next->next->prev = current_node;
+	deleted_node->prev->next = deleted_node->next;
+
+	if (deleted_node->next != NULL)
+		deleted_node->next->prev = deleted_node->prev;
+
 	free(deleted_node);
 
 	return (1);
