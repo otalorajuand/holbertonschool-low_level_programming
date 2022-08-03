660d0101c2ac6b673f266e614b1497d919929983
otalorajuand <otalorajuand@gmail.com>
2022-08-02 17:29:57 -0700

    Addition of task 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,8 +20,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->next = *head;
 	new->prev = NULL;
 
-	if (*(head)->next != NULL)
-		(*head)->next->prev = new;
+	if (*head != NULL)
+	{
+		if ((*(head))->next != NULL)
+			((*head))->next->prev = new;
+	}
 
 	*head = new;
 
