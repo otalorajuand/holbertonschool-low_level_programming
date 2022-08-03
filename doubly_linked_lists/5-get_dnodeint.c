bc274e3ed0f7c19e0aa4de7031e0d4733d7e73e2
otalorajuand <otalorajuand@gmail.com>
2022-08-02 18:11:28 -0700

    addition of task 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index - finds the indexth node  of a dlistint_t list
+  * @head: The head node of the list.
+  * @index: the index..
+ * Return: he address of the new element, or NULL if it failed.
+  */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *node;
+
+	if (head == NULL)
+		return (NULL);
+
+	node = head;
+
+	while (node != NULL && i < index)
+	{
+		node = node->next;
+		i++;
+	}
+
+	return (node);
+}
