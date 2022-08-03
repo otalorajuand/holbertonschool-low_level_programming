6e9a91d3c14f1fb15f9979a46ce1f24ad7a7ecfe
otalorajuand <otalorajuand@gmail.com>
2022-08-03 13:17:41 -0700

    addition of task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,8 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	char c;
-	FILE *file1, *file2;
+	char *buffer;
+	int file1, file2, buff_size, len_f1;
 
 	if (argc != 3)
 	{
@@ -18,27 +18,31 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	file1 = fopen(argv[1], "r");
+	file1 = open(argv[1], O_RDONLY);
 
-	if (file1 == NULL)
+	if (file1 == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	
-	file2 = fopen(argv[2], "w");
 
-	if (file2 == NULL)
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (file2 == -1)
 	{
-		fclose(file1);
+		close(file1);
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
-	while ((c = fgetc(file1)) != EOF)
-		fputc(c, file2);
+	buff_size = 1024;
+	buffer = malloc(sizeof(char) * buff_size);
+	len_f1 = read(file1, buffer, buff_size);
 
-	fclose(file1);
-	fclose(file2);
+	write(file2, buffer, len_f1);
+
+	close(file1);
+	close(file2);
+
 	return (1);
 }
