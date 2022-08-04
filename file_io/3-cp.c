add99ce3a1b04d667e2e8ab3baae80a3fcf88619
otalorajuand <otalorajuand@gmail.com>
2022-08-03 17:00:49 -0700

    addition of task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 	close_fd = close(file2);
 	if (close_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d\n", file2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n", file2);
 		exit(100);
 	}
 	return (1);
