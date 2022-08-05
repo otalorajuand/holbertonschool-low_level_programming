72816a7f3df9f7da91a5b76f1f5b871764e1f802
otalorajuand <otalorajuand@gmail.com>
2022-08-04 17:52:27 -0700

    Addition of task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,46 +1,52 @@
 #include "main.h"
 
+/**
+  * main - a program that copies the content of a file to another file
+  * @argc: arguments count.
+  * @argv: arguments vector.
+ * Return: 1 if works.
+  */
+
 
 int main(int argc, char *argv[])
 {
-        char buffer[BUFSIZ];
-        int file1, file2, len_f1 = BUFSIZ, close_fd, write_fd;
+	char buffer[BUFSIZ];
+	int file1, file2, len_f1 = BUFSIZ, close_fd, write_fd;
 
-        if (argc != 3)
-        {
-                dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-                exit(97);
-        }
-        file1 = open(argv[1], O_RDONLY);
-        file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-        /* missing check error conditions */
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-        while (len_f1 == BUFSIZ)
-        {
-                len_f1 = read(file1, buffer, BUFSIZ);
-                if (len_f1 == -1)
-                {
-                        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-                        exit(98);
-                }
-                write_fd = write(file2, buffer, len_f1);
-                if (write_fd == -1)
-                {
-                        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-                        exit(99);
-                }
-        }
-        close_fd = close(file1);
-        if (close_fd == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd %d", file1);
-                exit(100);
-        }
-        close_fd = close(file2);
-        if (close_fd == -1)
-        {
-                dprintf(STDERR_FILENO, "Error: Can't close fd %d", file2);
-                exit(100);
-        }
-        return (0);
+	file1 = open(argv[1], O_RDONLY);
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	while (len_f1 == BUFSIZ)
+	{
+		len_f1 = read(file1, buffer, BUFSIZ);
+		if (len_f1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		write_fd = write(file2, buffer, len_f1);
+		if (write_fd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close_fd = close(file1);
+	if (close_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file1);
+		exit(100);
+	}
+	close_fd = close(file2);
+	if (close_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file2);
+		exit(100);
+	}
+	return (0);
 }
