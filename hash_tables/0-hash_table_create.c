50d8ee34616e88e6ab8d2f626758fdc90eb96962
otalorajuand <otalorajuand@gmail.com>
2022-08-30 18:28:20 +0000

    addition of task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	res->size = size;
-	res->array = malloc(sizeof(hash_node_t*) * size);
+	res->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (res->array == NULL)
 		return (NULL);
