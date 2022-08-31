a7fd9387e17a39cefa3866fd59204f7fdcf1b94d
otalorajuand <otalorajuand@gmail.com>
2022-08-31 16:38:14 +0000

    Addition of task 6

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,39 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - Creates a hash table node.
+ * @key: the key.
+ * @value: the value.
+* Return: The node.
+ */
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node = calloc(1, sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (0);
+
+	node->key = calloc(1024, sizeof(char));
+	if (node->key == NULL)
+	{
+		free(node);
+		return (0);
+	}
+	node->value = calloc(1024, sizeof(char));
+	if (node->value == NULL)
+	{
+		free(node);
+		free(node->key);
+		return (0);
+	}
+	strcpy(node->key, (char *)key);
+	strcpy(node->value, (char *)value);
+
+	return (node);
+}
+
+/**
  * hash_table_set - a function that adds an element to the hash table.
  * @ht: the hash table you want to add or update the key/value to
  * @key: the key of th ht.
@@ -12,43 +45,37 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index = 0;
-	hash_node_t *node = NULL;
+	hash_node_t *node = NULL, *tmp = NULL;
+	char *copy_value;
 
 	if (strcmp(key, "") == 0 || ht == NULL)
 		return (0);
 
-	node = calloc(1, sizeof(hash_node_t));
-
-	if (node == NULL)
-		return (0);
-
-	node->key = calloc(1024, sizeof(char));
-
-	if (node->key == NULL)
-	{
-		free(node);
-		return (0);
-	}
-
-	node->value = calloc(1024, sizeof(char));
-
-	if (node->value == NULL)
-	{
-		free(node);
-		free(node->key);
-		return (0);
-	}
-	strcpy(node->key, (char *)key);
-	strcpy(node->value, (char *)value);
-
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL || strcmp(ht->array[index]->key, key) == 0)
+
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
-		ht->array[index] = node;
-	} else
-	{
-		node->next = ht->array[index];
-		ht->array[index] = node;
+		if (strcmp(tmp->key, key) == 0)
+		{
+			copy_value = strdup(value);
+			if (copy_value == NULL)
+			{
+				fprintf(stderr, "Error: duplicating value\n");
+				return (0);
+			}
+			free(tmp->value);
+			tmp->value = copy_value;
+			return (1);
+		}
+		tmp = tmp->next;
 	}
+	node = create_node(key, value);
+	if (node == NULL)
+	{
+		return (0);
+	}
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
