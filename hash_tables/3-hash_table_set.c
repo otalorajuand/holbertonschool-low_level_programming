3d187df9e3ce61184e2e497ff1673d4dfe626630
otalorajuand <otalorajuand@gmail.com>
2022-08-31 00:23:39 +0000

    addition of task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL)
+	if (ht->array[index] == NULL || strcmp(ht->array[index]->key, key) == 0)
 	{
 		ht->array[index] = node;
 	} else
