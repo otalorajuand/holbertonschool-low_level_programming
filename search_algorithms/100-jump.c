f91244aaed7333bd0845087592e06f5be7db66f3
otalorajuand <otalorajuand@gmail.com>
2022-09-10 00:15:23 +0000

    Addition of task 100

---
+++ search_algorithms/100-jump.c
@@ -0,0 +1,64 @@
+#include "search_algos.h"
+
+/**
+ * _min - Finds the minimum between two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+* Return: The minimum between the integers.
+ */
+
+int _min(int a, int b)
+{
+	if (a > b)
+		return (b);
+	else
+		return (a);
+}
+
+/**
+ * jump_search - searches for a value in a sorted array of integers
+ * using the Jump search algorithm
+ * @array: A pointer to the first element of the array.
+ * @size: The size of the array.
+ * @value: The value to find in the array.
+* Return: The index of the value, or -1 if it isn't in the array.
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int jump = 0, lo = 0, hi = 0;
+
+	jump = floor(sqrt((int)size));
+	hi = jump;
+
+	printf("Value checked array[%d] = [%d]\n", lo, array[lo]);
+	while (array[_min(hi, (int)size) - 1] < value)
+	{
+		lo = hi;
+		hi += jump;
+		if (lo >= (int)size)
+		{
+			hi = lo;
+			lo -= jump;
+			printf("Value found between indexes [%d] and [%d]\n", lo, hi);
+			printf("Value checked array[%d] = [%d]\n", lo, array[lo]);
+			return (-1);
+		}
+		printf("Value checked array[%d] = [%d]\n", lo, array[lo]);
+	}
+
+	printf("Value found between indexes [%d] and [%d]\n", lo, hi);
+	printf("Value checked array[%d] = [%d]\n", lo, array[lo]);
+	while (array[lo] < value)
+	{
+		lo++;
+		if (lo == _min(hi, (int)size))
+			return (-1);
+		printf("Value checked array[%d] = [%d]\n", lo, array[lo]);
+	}
+
+	if (array[lo] == value)
+		return (lo);
+
+	return (-1);
+}
